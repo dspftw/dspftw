9bae874b03c2242af0355a7cbd64c23c590c8dd3
Bill Allen <photo.allen@gmail.com>
2021-02-03 08:35:04 -0700

    Removed old test.

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -10,12 +10,3 @@ class PlottingTests(unittest.TestCase):
         complex_data = np.array([1+0j, 0+1j, -1+0j, 0-1j])
         fig = dspftw.plot_complex(complex_data)
         self.assertNotEqual(fig, None)
-
-    def test_plot_signal(self):
-        def my_sinusoid(t): return dspftw.complex_sinusoid(A=5, f=5, t=t, phi=0)
-
-        times = np.linspace(0, 1, num=25)
-        my_signal = my_sinusoid(times)
-
-        fig = dspftw.plot_signal(my_signal, times)
-        self.assertNotEqual(fig, None)
