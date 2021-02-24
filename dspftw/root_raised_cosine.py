2a9c83d4ffc9200b432b0eaff79ae9bdcf8d7237
Rick Faber <faber@rick-fabers-macbook-2.local>
2021-02-24 08:05:33 -0700

    Added root_raised_cosine.py and root_raised_cosine_filter_generator.py

---
+++ dspftw/root_raised_cosine.py
@@ -0,0 +1,50 @@
+# vim: expandtab tabstop=4 shiftwidth=4
+
+from numpy import pi, sin, cos, array, where, zeros
+from numpy import array as nparray
+
+def root_raised_cosine(time_array: nparray, Ts: float, beta: float=0.25) -> nparray:
+    '''
+    Compute the values of the root raised cosine function at points in time_array.
+
+    Parameters
+    ----------
+    time_array: nparray
+        Input time values.
+    Ts: float
+        Symbol period in seconds.  Reciprocal of the symbol rate.
+    beta: float
+        Roll off factor.  In the interval (0,1]
+        Default is 0.25.
+
+    Returns a numpy float array.
+    '''
+
+    # Define threshhold for zero
+    zr = 10**-10
+
+    # Create output array
+    func_val = zeros(len(time_array))
+
+    # Initial denominator values
+    denominator = pi*time_array/Ts*(1-16*(beta**2)*(time_array**2)/(Ts**2))
+
+    # Compute output at points where denominator1 is not zero
+    nzidx = where(abs(denominator) >= zr)
+    numerator1 = (sin(pi*time_array[nzidx]/Ts*(1-beta)) + 4*beta*time_array[nzidx]/Ts*cos(pi*time_array[nzidx]/Ts*(1+beta)))
+    denominator1 = denominator[nzidx]
+    func_val[nzidx] = numerator1/denominator1
+
+    # Compute output at points where denominator1 is zero
+    zidx = where(abs(denominator) < zr)
+    numerator2 = pi*(1-beta)*cos(pi*time_array[zidx]*(1-beta)/Ts)/Ts + 4*beta*cos(pi*time_array[zidx]*(1+beta)/Ts)/Ts - 4*pi*time_array[zidx]*beta*(1+beta)*sin(pi*time_array[zidx]*(1+beta)/Ts)/(Ts**2)
+    denominator2 = pi*((Ts**2)-48*(time_array[zidx]**2)*(beta**2))/(Ts**3)
+    func_val[zidx] = numerator2/denominator2
+
+    return func_val
+
+def rrcfunct(*args, **kwargs) -> nparray:
+    '''
+    Alias for root_raised_cosine.
+    '''
+    return root_raised_cosine(*args, **kwargs)
---
+++ dspftw/root_raised_cosine_filter_generator.py
@@ -0,0 +1,50 @@
+# vim: expandtab tabstop=4 shiftwidth=4
+
+from numpy import pi, sin, cos, ceil, floor, append, arange, where, zeros
+from numpy import array as nparray
+from root_raised_cosine import *
+
+def root_raised_cosine_filter_generator(symbol_width: int, symbol_rate: float, sample_rate: float, beta: float=0.25) -> nparray:
+    '''
+    Compute the taps of a root raised cosine (RRC) filter.
+
+    Parameters
+    ----------
+    symbol_width: int
+        Number of bauds wide for the output filter.  An odd integer will ensure the initial and final taps are at baud boundaries.
+    symbol_rate: float
+        Symbol rate in Hz.
+    sample_rate: float
+        Sample rate in Hz.
+    beta: float
+        Roll off factor.  In the interval (0,1]
+        Default is 0.25.
+
+    Returns a numpy float array.
+    '''
+
+    # Set the symbol period
+    Ts = 1.0/symbol_rate
+
+    # Create the input time values
+    time_array = arange(-ceil((symbol_width/2.0)*Ts*sample_rate), ceil((symbol_width/2.0)*Ts*sample_rate+1), dtype=float)/sample_rate
+
+    # Create filter values
+    if beta == 0:
+        filter_val = zeros(time_array.shape)
+        tlen = len(time_array)
+        # Define filter everywhere except center tap
+        idx = append(arange((tlen-1)//2, dtype=int), arange((tlen-1)//2+1, tlen, dtype=int))
+        filter_val[idx] = sin(pi*time_array[idx]/Ts) / (pi*time_array[idx]/Ts)
+        # Define filter at center tap
+        filter_val[(tlen-1)//2] = 1
+    else:
+        filter_val = root_raised_cosine(time_array,Ts,beta)
+
+    return filter_val
+
+def rrcfiltgen(*args, **kwargs) -> nparray:
+    '''
+    Alias for root_raised_cosine_filter_generator.
+    '''
+    return root_raised_cosine_filter_generator(*args, **kwargs)
