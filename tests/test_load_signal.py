0b44028c71bca845bbd86236676aaf26aa6dd770
Bill Allen <photo.allen@gmail.com>
2021-03-03 10:29:53 -0700

    Added more checks for load_signal.

--- tests/test_load_signal.py
+++ tests/test_load_signal.py
@@ -1,5 +1,7 @@
 # vim: expandtab tabstop=4 shiftwidth=4
 
+from numpy import array as nparray
+from numpy import isnan
 from pathlib import Path
 from subprocess import getstatusoutput
 from tempfile import TemporaryDirectory
@@ -11,8 +13,11 @@ import dspftw
 
 FILE_DIR = os.path.dirname(os.path.realpath(__file__))
 
+def has_nan(signal: nparray) -> bool:
+    return any(isnan(signal))
+
 class LoadSignalTests(unittest.TestCase):
-    def generate_signal(self, signal_type: str, endianness: str) -> None:
+    def generate_signal(self, signal_type: str, endianness: str) -> nparray:
         with TemporaryDirectory() as tempdir:
             working_dir = Path(tempdir)
             output_path = working_dir.joinpath('test_file.'+signal_type)
@@ -23,6 +28,12 @@ class LoadSignalTests(unittest.TestCase):
             self.assertEqual(octave_status, 0)
             return dspftw.load_signal(output_path, signal_type, 'cplx', endianness)
 
-    def test_load_signal(self):
-        sig = self.generate_signal('32f', 'little')
-        self.assertGreater(len(sig), 0)
+    def test_load_32fl_signal(self):
+        signal = self.generate_signal('32f', 'little')
+        self.assertEqual(len(signal), 10_000)
+        self.assertFalse(has_nan(signal))
+
+    def test_load_8t_signal(self):
+        signal = self.generate_signal('8t', 'big')
+        self.assertEqual(len(signal), 10_000)
+        self.assertFalse(has_nan(signal))
