7ee8de7ab444ba8104ceb21ac277eb3969ab6e0e
quitosaras <79557765+quitosaras@users.noreply.github.com>
2021-03-03 09:29:42 -0700

    Update signal_correlation.py

--- dspftw/signal_correlation.py
+++ dspftw/signal_correlation.py
@@ -4,20 +4,20 @@ import numpy as np
 import scipy as ss
 
 def signal_correlation(in1, in2, norm=True):
-	'''Cross-correlates two arrays and normalizes the output'''
-	arrL = in1
-	arrS = in2
-	# If norm is set to False, unnormalized output will be returned
-	outArr = ss.correlate(arrL, arrS, mode='full')
-	# Default output is set to normalized
-	if norm:
-		sPow = np.sqrt(np.sum(arrS*arrS.conj()))
-		lPow = np.sqrt(np.convolve(arrL*arrL.conj(), np.ones(len(arrS)), mode='full'))*sPow
-		outArr = outArr/lPow
-	return outArr
+    '''Cross-correlates two arrays and normalizes the output'''
+    arrL = in1
+    arrS = in2
+    # If norm is set to False, unnormalized output will be returned
+    outArr = ss.correlate(arrL, arrS, mode='full')
+    # Default output is set to normalized
+    if norm:
+        sPow = np.sqrt(np.sum(arrS*arrS.conj()))
+        lPow = np.sqrt(np.convolve(arrL*arrL.conj(), np.ones(len(arrS)), mode='full'))*sPow
+        outArr = outArr/lPow
+    return outArr
 
 def sigcorr(*args, **kwargs):
-	'''
-	Alias for signal_correlation.
-	'''
-	return signal_correlation(*args, **kwargs)
+    '''
+    Alias for signal_correlation.
+    '''
+    return signal_correlation(*args, **kwargs)
