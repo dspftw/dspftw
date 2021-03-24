15fc8688de84e0f7ca6fba827f2969864b32e71f
Bill Allen <photo.allen@gmail.com>
2021-03-24 10:12:50 -0600

    Added save_bits and tests.

---
+++ tests/test_save_load_bits.py
@@ -0,0 +1,26 @@
+# vim: expandtab tabstop=4 shiftwidth=4
+
+from math import ceil
+from pathlib import Path
+from random import choice
+from tempfile import TemporaryDirectory
+
+import unittest
+
+import dspftw
+
+class SaveLoadBitsTests(unittest.TestCase):
+    def test_save_load_packed(self):
+        with TemporaryDirectory() as tempdir:
+            temp_path = Path(tempdir)
+            temp_file = temp_path.joinpath('bits_file')
+
+            bits = [choice((True, False)) for i in range(987)]  # length not a multiple of 8
+
+            num_bytes_written = dspftw.save_bits(str(temp_file), bits)
+            self.assertEqual(num_bytes_written, ceil(len(bits) / 8.0))
+
+            loaded_bits = dspftw.load_bits(str(temp_file))
+
+            for i in range(len(bits)):
+                self.assertEqual(bits[i], loaded_bits[i])
